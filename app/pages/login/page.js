5344adea913dd31c9ba983e2f11a06440ec326bd
H-Taliby <habibataliby@gmail.com>
2025-11-26 20:44:51 +0100

    added credentials include for the login request

--- app/pages/login/page.js
+++ app/pages/login/page.js
@@ -27,7 +27,7 @@ export default function Login() {
 
         try {
             const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/auth/login`, {
-                headers: { 'Content-Type': 'application/json' },
+                headers: { 'content-Type': 'application/json' },
                 method: "POST",
                 credentials: 'include', 
                 body: JSON.stringify({ email, password })
