56f390ee0c57f2697af947bdcdb63a690a859f37
H-Taliby <habibataliby@gmail.com>
2025-11-27 20:11:28 +0100

    added credentials include for the login request

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -63,7 +63,7 @@ export default function Header() {
             }
             )
         } catch (err) {
-            next(err)
+           console.log(err);
         }
     }
     useEffect(() => {
