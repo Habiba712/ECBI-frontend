2fde78853477b9da9b6196d525c6d3dea2d8c2a1
H-Taliby <habibataliby@gmail.com>
2025-11-28 23:14:47 +0100

    linked the scan to the api

---
+++ app/pages/posts/createPost/[id]/page.js
@@ -0,0 +1,13 @@
+
+
+"use client";
+
+import { useState, useEffect } from "react";
+export default function CreatePostForPos() {
+
+    return(
+        <div>
+            <h1>Create Post For Pos</h1>
+        </div>
+    )
+}
