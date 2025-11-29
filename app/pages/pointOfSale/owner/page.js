fd37b571d9d4639267341cf5d8437aa9efd14aa2
H-Taliby <habibataliby@gmail.com>
2025-11-29 01:47:18 +0100

    linked the scan to the api

--- app/pages/pointOfSale/owner/page.js
+++ app/pages/pointOfSale/owner/page.js
@@ -168,16 +168,16 @@ useEffect(() => {
 
         return(
              <div 
-             key={index || item.id}
-             className="mt-4 rounded-lg shadow-lg flex flex-col gap-5 justify-between items-start p-4 w-[100%] h-[100%]">
+             key={index || item?._id}
+             className="mt-4 rounded-lg shadow-lg flex flex-col gap-5 justify-between item?s-start p-4 w-[100%] h-[100%]">
                 <div className="flex justify-between h-full w-full">
                     <div className="h-full w-2/3 flex-grow">
-                        <Image src={item.coverImage}alt="restaurant" width={600} height={600} className="rounded-lg h-fit w-full object-cover flex-grow" />
+                        <Image src={item?.coverImage}alt="restaurant" width={600} height={600} className="rounded-lg h-fit w-full object-cover flex-grow" />
                     </div>
                     <div className=" w-1/3 grid grid-cols-2 gap-3 flex-grow ">
                       
                         <div className=" w-full">
-                             <h3 className="ml-4 text-black font-semibold">{item.name}</h3>
+                             <h3 className="ml-4 text-black font-semibold">{item?.name}</h3>
                         <div className="flex flex-col gap-2 mt-5 w-full">   
                            
                                         <p className="ml-4 font-semibold w-full flex gap-2"
@@ -187,12 +187,12 @@ useEffect(() => {
                                            }}
                                         >
                                             <ZipCodeIcon className={'w-5 h-5'}/>
-                                            {item.address.country}</p> 
+                                            {item?.address.country}</p> 
                                             <div className="ml-1">
-                                                 <p className="ml-4 text-sm">{item.address.state}</p>
-                                         <p className="ml-4 text-sm">{item.address.city}</p> 
-                                          <p className="ml-4 text-sm">{item.address.street}</p>
-                                           <p className="ml-4 text-sm">{item.address.zipCode}</p>
+                                                 <p className="ml-4 text-sm">{item?.address.state}</p>
+                                         <p className="ml-4 text-sm">{item?.address.city}</p> 
+                                          <p className="ml-4 text-sm">{item?.address.street}</p>
+                                           <p className="ml-4 text-sm">{item?.address.zipCode}</p>
                                             </div>
                                            
                                            <p className="ml-4 text-sm  w-full flex  font-semibold gap-2"
@@ -201,7 +201,7 @@ useEffect(() => {
                                            
                                            }}>
                                             <PhoneIcon className={'w-5 h-8'}/>
-                                            {item.phone}</p>
+                                            {item?.phone}</p>
                                             <p className="ml-4">
                                                   <a 
                                            className="font-semibold  hover:underline w-full flex gap-2"
@@ -209,7 +209,7 @@ useEffect(() => {
                                             fontSize:'13px'
                                              
                                            }}
-                                           href={item.website} target="_blank">
+                                           href={item?.website} target="_blank">
                                             <WebsiteIcon className={'w-5 h-5'}/>
                                             Website</a>
                                             </p>
@@ -219,7 +219,7 @@ useEffect(() => {
                                             fontWeigh:'normal'
                                            }} className="ml-4 text-sm w-full font-semibold flex gap-2">
                                             <StarIcon className={'w-8 h-8 text-yellow-500 fill-current'}/>
-                                            Specialises in {item.cuisine} cuisine</p>
+                                            Specialises in {item?.cuisine} cuisine</p>
 
                                         
                             
@@ -240,16 +240,16 @@ useEffect(() => {
                 <div className="flex flex-col gap-3 w-full">
                     <div className="grid grid-cols-3 gap-3 items-center">
                         <div className="rounded-lg bg-purple-100 px-3 py-2 w-full flex flex-col justify-center items-center">
-                            <span className="font-semibold text-purple-400"> {item.stats.totalVisits}</span>
+                            <span className="font-semibold text-purple-400"> {item?.stats.totalVisits}</span>
                             <p className="text-sm">Total Visits</p>
 
                         </div>
                         <div className="rounded-lg bg-green-100 px-3 py-2 w-full flex flex-col justify-center items-center">
-                            <span className="font-semibold text-green-400">{item.stats.averageRating}</span>
+                            <span className="font-semibold text-green-400">{item?.stats.averageRating}</span>
                             <p className="text-sm">Ratings</p>
                         </div>
                         <div className="rounded-lg bg-blue-100 px-3 py-2 w-full flex flex-col justify-center items-center">
-                            <span className="font-semibold text-blue-400">{item.stats.totalReviews}</span>
+                            <span className="font-semibold text-blue-400">{item?.stats.totalReviews}</span>
                             <p className="text-sm">Reviews</p>
                         </div>
 
@@ -273,7 +273,7 @@ useEffect(() => {
                         </div> */}
                         <div className="flex justify-center bg-blue-100 rounded-lg py-1 px-2 text-sm hover:scale-110 transition-all ease-in-out duration-500 ">
                             <button className="flex gap-2 items-center cursor-pointer "
-                            onClick={()=>redirect(`/pages/pointOfSale/owner/${item._id}`)}
+                            onClick={()=>redirect(`/pages/pointOfSale/owner/${item?._id}`)}
                             >
                                 <ViewIcon className="w-6 h-6 text-blue-400" />
                                 View</button>
