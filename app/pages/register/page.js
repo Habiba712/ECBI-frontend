027521014936226d9b0a8a6b611ce4c9871c59b6
H-Taliby <habibataliby@gmail.com>
2026-01-10 15:04:05 +0100

    new backend deployment

--- app/pages/register/page.js
+++ app/pages/register/page.js
@@ -130,6 +130,7 @@ export default function Login() {
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             /></div>
+                            
 
                         </div>
 
