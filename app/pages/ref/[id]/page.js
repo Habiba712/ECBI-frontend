6c81712a7da6ea49b14de93f0da5ede4e1b9fe0c
H-Taliby <habibataliby@gmail.com>
2026-01-12 23:34:50 +0100

    referral page to test the clicked shared link from a user

---
+++ app/pages/ref/[id]/page.js
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function ReferralPage() {
+  const router = useRouter();
+  const { linkId } = router.query;
+
+  useEffect(() => {
+    if (!linkId) return;
+
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/referralLink/getReferralLinkByLink/${linkId}`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .finally(() => {
+        router.replace("/"); // or /pos/[id]
+      });
+
+  }, [linkId]);
+
+  return <p>Redirecting…</p>;
+}
