79cde4636bd01a2c8113a209e41036b2a202218a
H-Taliby <habibataliby@gmail.com>
2026-01-18 18:05:03 +0100

    fetch referral link for User B, from User A, for Pos A

--- app/components/modals/addPost.js
+++ app/components/modals/addPost.js
@@ -14,7 +14,7 @@ import { useRouter } from "next/navigation";
 export default function AddPost() {
 const {id} = useParams();
 const router = useRouter();
-const [owner, setOwner] = useState("");
+const [owner, setOwner] = useState(null);
 const [isModalOpen, setIsModalOpen] = useState(true);
 const [photoURL, setPhotoURL] = useState("");
 const [postPicToAdd, setPostPicToAdd] = useState(null);
