7d90cfe29bb7008b7b7a5310fdf8740a09861c6e
H-Taliby <habibataliby@gmail.com>
2026-05-21 16:56:26 +0100

    fetching notifications for one receipient

--- app/pages/notif/page.js
+++ app/pages/notif/page.js
@@ -12,38 +12,40 @@ export default function Notifications() {
     const [notifCount, setNotifCount] = useState(0);
     const [userId, setUserId] = useState();
     const [token, setToken] = useState("");
-    
+
     const getNotifs = async () => {
-        try{
+        try {
+            
             const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/notif/getByReceipient/${userId}`, {
+                method: "GET",
                 headers: {
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${token}`,
                 }
             }).then((res) => {
-                if(res.ok){
-                    res.json().then((data) =>{
+                if (res.ok) {
+                    res.json().then((data) => {
                         console.log('data', data);
                         setNotifs(data);
                         setNotifCount(data?.length);
                     })
                 }
             })
-        }catch(err){
+        } catch (err) {
             console.log('error fetching notifs', err);
         }
     }
 
-       useEffect(() => {
-            const session = JSON.parse(localStorage.getItem("sessionData")) || null;
-            // console.log('session', session?.userId);
-            setUserId(session?.userId);
-            setToken(session?.token);
-        }, []);
-        useEffect(()=>{
-            getNotifs();
-        },[userId, token])
-        console.log('userId', userId);
-        console.log('my notifs', notifs);
+    useEffect(() => {
+        const session = JSON.parse(localStorage.getItem("sessionData")) || null;
+        // console.log('session', session?.userId);
+        setUserId(session?.userId);
+        setToken(session?.token);
+    }, []);
+    useEffect(() => {
+        getNotifs();
+    }, [userId, token])
+    console.log('userId', userId);
+    console.log('my notifs', notifs);
     return (
         <section className="min-h-screen h-full max-w-md mx-auto flex flex-col w-full mb-20">
             <div className="p-4 flex justify-between items-center dashboard-page mb-5 mt-5 border-b-3 border-gray-100">
@@ -59,6 +61,52 @@ export default function Notifications() {
                 </div>
 
             </div>
+
+            {/* notifs list */}
+             <div className="flex justify-between w-full">
+                            <h2 className="font-bold text-md px-4 font-gray-800"> Last 7 days</h2>
+             </div>
+            {
+               notifs?.length > 0 ? (
+                   <div className="flex flex-col gap-3 p-4 text-sm " >
+                        {
+                            notifs.map((notif) => (
+                                <div key={notif._id}  className="flex items-start px-2 py-4 rounded-lg shadow-lg w-full cursor-pointer hover:bg-gray-100">
+                                    <div className="flex justify-start justify-center cover-image  w-10">
+                                        <Image src={notif?.sender?.base?.avatar} alt="avatar" width={70} height={60} className="rounded-full" />
+                                        </div>
+                                    
+                                    <div className="flex flex-col items-start w-70">
+                                        <p className="px-2 font-bold  ">{notif?.message}</p>
+                                        <span style={{ fontSize: "12px" }} className="text-gray-600 text-sm px-2">
+                                            {notif?.sender?.base?.name}
+                                        </span>
+                                        </div>
+                                    
+                                    <div className="flex items-start justify-center w-30 ">
+                                        <span 
+                                        style={{ fontSize: "10px" }}
+                                        className="font-semibold flex justify-start items-center text-gray-600 text-sm">
+                                            {new Date(notif?.createdAt).toLocaleString('en-US', {
+                                                month: 'numeric',
+                                                day: 'numeric',
+                                                year: 'numeric',
+                                                hour: 'numeric',
+                                                minute: 'numeric',
+                                            
+                                            })}
+                                        </span>
+                                        </div>
+                                </div>
+                            ))
+
+                        }
+
+                    </div>
+               )
+               : 
+               null
+            }
 
 
         </section>
