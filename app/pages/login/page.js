33e49ce80322970a5e7cf4b3a02850b7aa8e054d
H-Taliby <habibataliby@gmail.com>
2026-06-30 00:14:43 +0100

    just doign the bare minimum

--- app/pages/login/page.js
+++ app/pages/login/page.js
@@ -57,14 +57,7 @@ export default function Login() {
 
     }
     return (
-           <motion.div 
-                            className="w-full h-full border flex items-center  p-4 overflow-y-auto scrollbar-thin"
-                            initial={{ y: "100%" }}
-                            animate={{ y: 0 }}
-                            exit={{ y: "100%" }}
-                            transition={{ type: "spring", damping: 25, stiffness: 250 }}
-                            onClick={(e) => e.stopPropagation()} // Prevents closing when clicking modal content
-                        > <section className="flex flex-col items-center justify-center mx-auto max-w-md  text-gray-300 border-gray-300 rounded-lg shadow-lg "
+          <section className="flex flex-col items-center justify-center mx-auto max-w-md  text-gray-300 border-gray-300 rounded-lg shadow-lg overflow-hidden"
             style={{
                 "background": "bg-[linear-gradient(135deg,#6D5BFF_0%,#8A7CFF_35%,#A78BFA_70%,#60A5FA_100%)]",
                 "WebkitBackdropFilter": "blur(20px)",
@@ -135,7 +128,7 @@ export default function Login() {
                         
            
         </section>
-                        </motion.div>
+                     
       
                         
         
