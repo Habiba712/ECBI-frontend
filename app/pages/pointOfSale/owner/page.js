e9794850ace3c7e1d8b411bfe204d1d68d38944c
H-Taliby <habibataliby@gmail.com>
2026-06-29 18:19:30 +0100

    just doign the bare minimum

--- app/pages/pointOfSale/owner/page.js
+++ app/pages/pointOfSale/owner/page.js
@@ -417,8 +417,8 @@ export default function PointOfSale() {
   )
 
   return (
-    <section className="min-h-screen bg-gray-50">
-      <div className="max-w-5xl mx-auto px-4 py-6">
+    <section className=" px-4 py-6 w-full">
+      <div className="w-full">
 
         
         <div className="flex items-center justify-between mb-6">
