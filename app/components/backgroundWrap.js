57c500eaac8911d13de09e440f1b9db5469f6d96
H-Taliby <habibataliby@gmail.com>
2026-06-30 15:46:39 +0100

    idk what am doing

--- app/components/backgroundWrap.js
+++ app/components/backgroundWrap.js
@@ -1,27 +1,39 @@
+// app/BackgroundWrapper.jsx
 'use client'
-
-import { usePathname } from 'next/navigation';
-
+import { usePathname } from 'next/navigation'
+import bg_image from '../../public/fancy_resto_bg.webp'
 export default function BackgroundWrapper({ children }) {
   const pathname = usePathname();
+  if(pathname.includes("/register") || pathname.includes("/login") || pathname.includes("/createOwner") || pathname.includes("/password")){
+return (
+   <div
+     style={{
+   
+    
+    minHeight: '100vh',
 
-  const isAuthPage =
-    pathname.includes("/register") ||
-    pathname.includes("/login") ||
-    pathname.includes("/createOwner") ||
-    pathname.includes("/password");
 
-  if (isAuthPage) {
-    return (
-      <div className="min-h-screen w-full flex items-center justify-center bg-[linear-gradient(135deg,#6D5BFF_0%,#8A7CFF_35%,#A78BFA_70%,#60A5FA_100%)] px-4">
-        {children}
-      </div>
-    );
-  }
-
-  return (
-    <div className="w-full">
+  }}
+  className='flex justify-center items-center bg-[linear-gradient(135deg,#6D5BFF_0%,#8A7CFF_35%
+,#A78BFA_70%,#60A5FA_100%)]'
+    >
       {children}
     </div>
-  );
+   
+  )
+
+
+  
 }
+else{
+  return (
+    <div
+      
+      className='flex justify-center items-center'
+    >
+      {children}
+    </div>
+
+  )
+
+}}
