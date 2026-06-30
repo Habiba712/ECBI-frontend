e78df2bddb14c70fb74bd35fa4514e3d74719456
H-Taliby <habibataliby@gmail.com>
2026-06-30 14:59:00 +0100

    just doign the bare minimum

--- app/components/backgroundWrap.js
+++ app/components/backgroundWrap.js
@@ -6,28 +6,16 @@ export default function BackgroundWrapper({ children }) {
   const pathname = usePathname();
   if(pathname.includes("/register") || pathname.includes("/login") || pathname.includes("/createOwner") || pathname.includes("/password")){
 return (
-   <div
-     style={{
-   
-    
-    minHeight: '100vh',
-
-
-  }}
-  className='flex justify-center items-center bg-[linear-gradient(135deg,#6D5BFF_0%,#8A7CFF_35%,#A78BFA_70%,#60A5FA_100%)]'
+   <div className="flex h-[100%] justify-center items-center bg-[linear-gradient(135deg,#6D5BFF_0%,#8A7CFF_35%,#A78BFA_70%,#60A5FA_100%)]"
     >
       {children}
     </div>
    
   )
-
-
-  
 }
 else{
   return (
     <div
-      
       className='flex justify-center items-center'
     >
       {children}
